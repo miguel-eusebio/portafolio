e19b59b55d73cd935ec6ec7195c57bda51d24086
Miguel Eusebio <miguel.eusebio117@gmail.com>
2023-03-12 15:39:53 -0600

    update portafolio 12/03/23

--- main.js
+++ main.js
@@ -7,31 +7,29 @@ window.onload = function (event) {
 
 App.prototype.processingButton = function(event) {
     const btn = event.currentTarget;
-    const slickList = event.currentTarget.parentNode;
+    const cardList = event.currentTarget.parentNode;
     const track = event.currentTarget.parentNode.querySelector('#track');
-    const slick = track.querySelectorAll('.slick');
+    const card = track.querySelectorAll('.card');
 
-    const slickWidth = slick[0].offsetWidth;
+    const cardWidth = card[0].offsetWidth;
     
     const trackWidth = track.offsetWidth;
-    const listWidth = slickList.offsetWidth;
+    const listWidth = cardList.offsetWidth;
 
     track.style.left == ""  ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
 
-    btn.dataset.button == "button-prev" ? prevAction(leftPosition,slickWidth,track) : nextAction(leftPosition,trackWidth,listWidth,slickWidth,track)
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition,cardWidth,track) : nextAction(leftPosition,trackWidth,listWidth,cardWidth,track)
 }
 
-let prevAction = (leftPosition,slickWidth,track) => {
+let prevAction = (leftPosition,cardWidth,track) => {
     if(leftPosition > 0) {
-        console.log("entro 2")
-        track.style.left = `${-1 * (leftPosition - slickWidth)}px`;
+        // console.log("entro 2")
+        track.style.left = `${-1 * (leftPosition - cardWidth)}px`;
     }
 }
 
-let nextAction = (leftPosition,trackWidth,listWidth,slickWidth,track) => {
+let nextAction = (leftPosition,trackWidth,listWidth,cardWidth,track) => {
     if(leftPosition < (trackWidth - listWidth)) {
-        track.style.left = `${-1 * (leftPosition + slickWidth)}px`;
+        track.style.left = `${-1 * (leftPosition + cardWidth)}px`;
     }
 }
-
-console.log('Hi');
